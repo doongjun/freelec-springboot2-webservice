67eecd28f660332aeae8d83e66420f1f5219d9db
doongjun <doongjun.kim@gmail.com>
2022-06-26 01:16:18 +0900

    Test: 게시글 수정

--- src/test/java/com/doongjun/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/doongjun/book/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.doongjun.book.springboot.web;
 import com.doongjun.book.springboot.domain.posts.Posts;
 import com.doongjun.book.springboot.domain.posts.PostsRepository;
 import com.doongjun.book.springboot.web.dto.PostsSaveRequestDto;
+import com.doongjun.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -37,7 +40,7 @@ public class PostsApiControllerTest {
     }
 
     @Test
-    public void posts_등록된다 () throws Exception {
+    public void posts_등록된다() throws Exception {
         //given
         String title = "hello world";
         String content = "안녕하세요.";
@@ -63,5 +66,39 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
         assertThat(all.get(0).getAuthor()).isEqualTo(author);
+    }
+
+    @Test
+    public void posts_수정된다() throws Exception {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("JPA 프로그래밍")
+                .content("hello JPA")
+                .author("김영한")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "스프링 부트와 AWS로 구현하는 웹 서비스";
+        String expectedContent = "hello Spring";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT,
+                requestEntity, Long.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 }
