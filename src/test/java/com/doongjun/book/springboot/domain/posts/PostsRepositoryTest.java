799b31ced8e6a40fa6fdee003d090a79b399a3b9
doongjun <doongjun@kakao.com>
2022-06-25 11:26:36 +0900

    Feat: 게시글 등록 api

--- src/test/java/com/doongjun/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/doongjun/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PostsRepositoryTest {
 
     @Autowired
-    PostRepository postRepository;
+    PostsRepository postRepository;
 
     @AfterEach
     public void cleanup() {
