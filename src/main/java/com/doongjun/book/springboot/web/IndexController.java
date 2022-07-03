d3fc941fdaadebfee3cac2c98cb48155823c26b4
doongjun <doongjun.kim@gmail.com>
2022-07-03 21:03:06 +0900

    Feat: 게시글 목록 조회

--- src/main/java/com/doongjun/book/springboot/web/IndexController.java
+++ src/main/java/com/doongjun/book/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.doongjun.book.springboot.web;
 
+import com.doongjun.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
